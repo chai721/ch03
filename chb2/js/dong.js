03ed962f90caaa473c4f20dbd91c824bfeecd4ef
chai721 <116002498+chai721@users.noreply.github.com>
2022-10-17 21:33:56 +0800

    Add files via upload

---
+++ chb2/js/dong.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" charset="utf-8" async=""
+		src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.0.min.js">
+</script>
+
+<script type="text/javascript"
+		src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.min.js?_=1557308476616"></script>
+
+<script type="text/javascript">
+		L2Dwidget.init();
+</script>
